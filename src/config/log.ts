bb63389298319a76ec11d0525a4fee6d77929dd3
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-11-15 10:55:03 +0100

    feat: ajout de log sur requêtes pour traçabilité des erreurs

--- src/config/log.ts
+++ src/config/log.ts
@@ -5,6 +5,6 @@ import { Request, Response, NextFunction } from "express"
 export const logSys = new log(params.serviceName.object.utilisateur, path.resolve("src", "log"))
 export const CE_Services = params.inAppServiceName
 export const LogRequest = (req : Request, res : Response, next : NextFunction) => {
-  const text = `${req.method} : ${req.baseUrl} | ${req.body} ; ${req.headers}`
+  const text = `${req.method} : ${req.baseUrl} | body : ${req.body} ; header : ${req.headers} | query : ${req.query}`
   logSys.ServiceInfo(params.inAppServiceName.app, text)
 }
