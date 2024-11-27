ba8f28ef2dedadbcf493300f5f32af8b3d4ebabc
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-11-27 14:29:13 +0100

    refactor(index): réécriture de certains commentaire et ajout d'espacement

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,71 @@
-import app from './app';
+import app from "./app";
 import os, { NetworkInterfaceInfo } from "os";
 import { params, utils, servicesConnexion } from "packages";
 
 const { FreePort } = utils;
 const { serviceName, inAppServiceName } = params;
-const { SignalAdressManager } = servicesConnexion
+const { SignalAdressManager } = servicesConnexion;
 
 /*
-CONNECT API
+
+  CONNECT API
+  
 */
 const main = async () => {
   /* Le système de log défini dans `app.ts` -> à voir dans le dossier ../package ou son ripo git */
-  let logSys = app.get("logSys")
-  let env = app.get("envLoad")
+  let logSys = app.get("logSys");
+  let env = app.get("envLoad");
 
-  if(!logSys) throw new Error("LogSys error : LogSys n'est pas monté dans le fichier `app.ts` sous le format `logSys`");
-  if(!env) throw new Error("Env error : Env n'est pas monté dans le fichier `app.ts` sous le format `envLoad`")
+  if (!logSys)
+    throw new Error(
+      "LogSys error : LogSys n'est pas monté dans le fichier `app.ts` sous le format `logSys`"
+    );
+  if (!env)
+    throw new Error(
+      "Env error : Env n'est pas monté dans le fichier `app.ts` sous le format `envLoad`"
+    );
 
-  try{
-    const port = await FreePort()
+  try {
+    const port = await FreePort();
 
     app.listen(port, env.IP_USER_SERVICE, () => {
-      const interfaces : NodeJS.Dict<NetworkInterfaceInfo[]> = os.networkInterfaces();
+      const interfaces: NodeJS.Dict<NetworkInterfaceInfo[]> =
+        os.networkInterfaces();
 
-      console.log("Connected to url :")
+      console.log("Connected to url :");
 
       for (const k in interfaces) {
-          for (const k2 in interfaces[k]) {
-              /* @ts-ignore */
-              const address = interfaces[k][k2];
-  
-              if (address.family === 'IPv4') {
-                logSys.ServiceInfo(inAppServiceName.app, `connected to ${address.address}:${port}`);
+        for (const k2 in interfaces[k]) {
+          /* @ts-ignore */
+          const address = interfaces[k][k2];
 
-                console.log(`${address.address}:${port}`);
+          if (address.family === "IPv4") {
+            logSys.ServiceInfo(
+              inAppServiceName.app,
+              `connected to ${address.address}:${port}`
+            );
 
-                /*
-                  CALL ADRESS MANAGER 
-                */
-                
-                SignalAdressManager({adressIP : address.address, port, service : serviceName.object.utilisateur}, env)
-              }
+            console.log(`${address.address}:${port}`);
+
+            /*
+              CALL ADRESS MANAGER 
+            */
+
+            SignalAdressManager(
+              {
+                service: serviceName.object.utilisateur,
+                adressIP: address.address,
+                port,
+              },
+              env
+            );
           }
+        }
       }
-
-    })
-  }catch(e : any){
-    logSys.UnknowAppError(inAppServiceName.index, e)
+    });
+  } catch (e: any) {
+    logSys.UnknowAppError(inAppServiceName.index, e);
   }
-}
+};
 
-main()
+main();
